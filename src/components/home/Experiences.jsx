ec3fcb757257d70e9b567c5b12f9909197352368
Lawrence Liu <lsk21007@gmail.com>
2022-08-06 20:58:05 -0700

    update resume

--- src/components/home/Experiences.jsx
+++ src/components/home/Experiences.jsx
@@ -6,7 +6,7 @@ const Experiences = ({ExperiencesRef}) => {
     {
       title1: "06-2022/ present - Front-End Deveoper \n IFENG (Auto channel)",
       phrase1: "1. Build new feature: \n Collaborated with senior-level developers to build a lucky draw feature and use React Router to link the new page to other web pages. \n 2. Maintain websites: \n Assisted the development team in maintaining websites including content updates, debugging, feature enhancements and testing.\n 3. Optimize compatibility: \n Developed cross-device compatibility and optimized mobile web layout.",
-      title2: "08-2014/ 08-2021 - Senior Product Planning Manager \n XCAR",
+      title2: "08-2014/ 08-2021 - Senior Web Planning Manager \n XCAR",
       desc2: "1. Webpage Designing:\n Design new channels (EV, Cross Country Channel) and features (Short Video Sharing), provide webpage layout, features and article types ideas to the web development department.\n 2. Website Analysing:\n Analyze users browsing preferences by utilizing the data of users' paths from the data platform. Provide the best plan for the client to cooperate.\n 3. Web Event Planning:\n According to the data analysis result, designing web activities to attract the users and promote the product for clients.",
     },
   ]
